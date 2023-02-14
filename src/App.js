c43532e7a6d6e3fb8238216eef683ea20d722b17
aadarshkumaran <aadarshkumar91@gmail.com>
2023-02-14 13:20:12 +0530

    add Completed feature in it

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let [list, setList] = useState([])//display all todo list
   let [title, setTitle] = useState("")//add new title
   let [description, setDescription] = useState("")//add new title
+  let [completed,setCompleted] = useState([])
 
   let handleAdd = () => {
     let newTask = {
@@ -33,9 +34,40 @@ function App() {
     setList(copiedTask)
   }
 
+  let handleDeleteCompleted = (index) =>{
+    let copiedCompletedTask = [...completed]
+    copiedCompletedTask.splice(index,1)
+    localStorage.setItem('completedtodolist',JSON.stringify(copiedCompletedTask))
+    setCompleted(copiedCompletedTask)
+  }
+
+  let handleComplete = (index)=>{
+    let now = new Date()
+    let dd = now.getDate()
+    let mm = now.getMonth()+1
+    let yyyy = now.getFullYear()
+    let h = now.getHours()
+    let m = now.getMinutes()
+    let s = now.getSeconds()
+    let completedTime = dd+"-"+mm+"-"+yyyy+" at "+h+":"+m+":"+s
+
+    let filteredTodo = {
+      ...list[index],
+      completedTime : completedTime
+    }
+
+    let copyCompleted = [...completed]
+    copyCompleted.push(filteredTodo)
+    setCompleted(copyCompleted)
+    handleDelete(index)
+    localStorage.setItem('completedtodolist', JSON.stringify(copyCompleted))
+  }
+
   useEffect(()=>{
     let fetchTodo = JSON.parse(localStorage.getItem(`todolist`))
+    let fetchCompleted = JSON.parse(localStorage.getItem(`completedtodolist`))
     if(fetchTodo) setList(fetchTodo) // if any todos are present, display all todos
+    if(fetchCompleted) setCompleted(fetchCompleted) // if any completed todos are present, display all of these
     
   },[])
 
@@ -62,6 +94,7 @@ function App() {
         </div>
         <div className="layer3">
           {
+            toggle === false &&
             list.map((data, index) => (
               <div className="todo-list" key={index}>
                 <div className="todo-content">
@@ -70,7 +103,22 @@ function App() {
                 </div>
                 <div className="todo-buttons">
                   <DeleteOutlineOutlinedIcon id='delete' onClick={()=>handleDelete(index)} />
-                  <DoneOutlineOutlinedIcon id='done' />
+                  <DoneOutlineOutlinedIcon id='done' onClick={()=>handleComplete(index)} />
+                </div>
+              </div>
+            ))
+          }
+          {
+            toggle === true &&
+            completed.map((data, index) => (
+              <div className="todo-list" key={index}>
+                <div className="todo-content">
+                  <h3>{data.title}</h3>
+                  <p>{data.description}</p>
+                  <span>Completed on: {data.completedTime}</span>
+                </div>
+                <div className="todo-buttons">
+                  <DeleteOutlineOutlinedIcon id='delete' onClick={()=>handleDeleteCompleted(index)} />
                 </div>
               </div>
             ))
